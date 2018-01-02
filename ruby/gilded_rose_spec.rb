85d3b9517622d25e5cd7c26a81812184ec0a7d97
Allan Wazacz <cazwazacz@gmail.com>
2018-01-02 18:31:34 +0000

    Add test which checks that quality degrades twice as fast after sell_in date has passed

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -8,6 +8,12 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "quality degrades twice as fast after sell_in date has passed" do
+      items = [Item.new("junk", 0, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 8
+    end
   end
 
 end
