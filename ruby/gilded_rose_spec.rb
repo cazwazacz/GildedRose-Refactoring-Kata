c0f0ada96e9e5d971900c7032b00b8203352f759
Allan Wazacz <cazwazacz@gmail.com>
2018-01-03 11:55:06 +0000

    Create ItemDouble and test for decrease in quality of Backstage passes

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,48 +1,82 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
+class ItemDouble
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+end
+
 describe GildedRose do
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+      items = [ItemDouble.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
 
     it "quality degrades twice as fast after sell_in date has passed" do
-      items = [Item.new("junk", 0, 10)]
+      items = [ItemDouble.new("junk", 0, 10)]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 8
     end
 
     it "quality is never negative" do
-      items = [Item.new("junk", 5, 5)]
+      items = [ItemDouble.new("junk", 5, 5)]
       10.times { GildedRose.new(items).update_quality() }
       expect(items[0].quality).to eq 0
     end
 
     it "quality of Aged Brie increases with time" do
-      items = [Item.new("Aged Brie", 10, 2)]
+      items = [ItemDouble.new("Aged Brie", 10, 2)]
       5.times { GildedRose.new(items).update_quality() }
       expect(items[0].quality).to eq 7
     end
 
     it "quality of an item can never increase above 50" do
-      items = [Item.new("Aged Brie", 10, 48)]
+      items = [ItemDouble.new("Aged Brie", 10, 48)]
       5.times { GildedRose.new(items).update_quality() }
       expect(items[0].quality).to eq 50
     end
 
     it "the sell_in value of Sulfuras never decreases" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+      items = [ItemDouble.new("Sulfuras, Hand of Ragnaros", 0, 80)]
       5.times { GildedRose.new(items).update_quality() }
       expect(items[0].sell_in).to eq 0
     end
 
     it "the quality of Sulfuras never changes" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+      items = [ItemDouble.new("Sulfuras, Hand of Ragnaros", 0, 80)]
       5.times { GildedRose.new(items).update_quality() }
       expect(items[0].quality).to eq 80
+    end
+
+    it "the quality of Backstage passes increases as time passes" do
+      items = [ItemDouble.new("Backstage passes to a TAFKAL80ETC concert", 20, 30)]
+      5.times { GildedRose.new(items).update_quality() }
+      expect(items[0].quality).to eq 35
+    end
+
+    it "the quality of Backstage passes increases by 2 when there are 10 or less days left" do
+      items = [ItemDouble.new("Backstage passes to a TAFKAL80ETC concert", 9, 30)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 32
+    end
+
+    it "the quality of Backstage passes increases by 3 when there are 5 or less days left" do
+      items = [ItemDouble.new("Backstage passes to a TAFKAL80ETC concert", 4, 30)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 33
+    end
+
+    it "the quality of Backstage passes iz zero when sell_in has passed" do
+      items = [ItemDouble.new("Backstage passes to a TAFKAL80ETC concert", 2, 30)]
+      5.times { GildedRose.new(items).update_quality() }
+      expect(items[0].quality).to eq 0
     end
   end
 
